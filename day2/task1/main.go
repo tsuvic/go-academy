73693f405a6ecd49adae8a9a8a4aec5c1ef01b25
tsuvi <admja11@gmail.com>
2022-10-31 22:05:21 +0900

    practice

--- day2/task1/main.go
+++ day2/task1/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println()
+	write()
+}
+
+func write() {
+	sc := bufio.NewScanner(os.Stdin)
+	fmt.Println("Input something you want to output to the file:")
+	sc.Scan()
+	text := sc.Text()
+	err := os.WriteFile("output.txt", []byte(text), os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func read() {
+
 }
