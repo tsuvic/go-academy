b7584de6e65bf7c23a075cf35d11fa1c1f5cefbc
tsuvi <admja11@gmail.com>
2022-11-15 22:06:00 +0900

    a

--- day2/practice4/main.go
+++ day2/practice4/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	ans := "2021年10月31日(日)2時0分 Sunday, October 31, 2021 2:00 AM"
-	println(ans)
-	fmt.Println("JAPAN", time.Now())
+	println("ANSWER\t", ans)
+	fmt.Println("JAPAN\t", time.Now())
 
 	New_York, _ := time.LoadLocation("America/New_York")
-	fmt.Println("NEW YORK", time.Now().In(New_York))
+	fmt.Println("NEWYORK\t", time.Now().In(New_York))
 
 	// HongKong, _ := time.LoadLocation("America/New_York")
 	// fmt.Println("HongKong", time.Now().In(HongKong))
